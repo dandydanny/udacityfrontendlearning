64035b1f32bf9f9dad9ed2691526770ad03619ce
Daniel W. Lu <dandydanny@gmail.com>
2018-02-13 19:37:31 -0500

    Added factorial exercise

---
+++ L13-14-4-7.js
@@ -0,0 +1,13 @@
+/*
+ * Programming Quiz: Factorials (4-7)
+ */
+
+// your code goes here
+var number = 12;
+var solution = 1;
+
+for (number; number > 1; number--) {
+    solution = solution * number;
+}
+
+console.log(solution);
