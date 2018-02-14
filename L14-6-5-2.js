ddfd66c72df83793ea1891f4b22c086b1916fec5
Daniel W. Lu <dandydanny@gmail.com>
2018-02-13 20:54:10 -0500

    Added laugh exercise

---
+++ L14-6-5-2.js
@@ -0,0 +1,20 @@
+/*
+ * Programming Quiz: Laugh it Off 2 (5-2)
+ *
+ * Write a function called `laugh` with a parameter named `num` that represents the number of "ha"s to return.
+ *
+ * Note:
+ *  - make sure your the final character is an exclamation mark ("!")
+ *  - make sure that your function produces the correct results when it is called multiple times
+ */
+
+function laugh(times) {
+	var response = "";
+	for (var iterations = 0; iterations < times; iterations++) {
+		response = response + "ha";
+	}
+	response = response + "!";
+	return response;
+}
+
+console.log(laugh(3));
