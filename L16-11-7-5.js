62b6011c9574125860367995c461be317323bd57
Daniel W. Lu <dandydanny@gmail.com>
2018-02-15 22:26:32 -0500

    Adding Facebook Friends quiz

--- L16-11-7-5.js
+++ L16-11-7-5.js
@@ -3,3 +3,20 @@
  */
 
 // your code goes here
+var facebookProfile = {
+	name: "Daniel",
+	friends: 100,
+	messages: ["Hello world!", "Hi Zucks!", "Why are the ads following me everywhere?"],
+	postMessage: function postMsg(msg) {
+		facebookProfile.messages.push(msg);
+	},
+	deleteMessage: function delMsg(index) {
+		facebookProfile.messages.splice(index,1);
+	},
+	addFriend: function addFriend() {
+		++facebookProfile.friends;
+	},
+	removeFriend: function removeFriend() {
+		--facebookProfile.friends;
+	}
+}
